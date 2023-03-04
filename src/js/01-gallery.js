8e93c279c80407a70f735ada7448a006315c355d
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-03-04 19:19:37 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+
+const items = galleryItems.map(item => {
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = item.original;
+
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery__image');
+    galleryImg.src = item.preview;
+    galleryImg.alt = item.description;
+    galleryImg.title = item.description;
+    
+    galleryLink.append(galleryImg);
+    return galleryLink;  
+});
+
+galleryContainerEl.append(...items);
+
+const lightbox = new SimpleLightbox(
+    '.gallery a',
+    { captionDelay: 250, scrollZoom: false }
+);
+
