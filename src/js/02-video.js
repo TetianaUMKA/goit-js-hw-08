70e3e0ee8e2147e03967864cb1f6f53349d6bf4b
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-03-04 21:30:08 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new vimeo(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+try {
+  const savedTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedTime) {
+    player.setCurrentTime(savedTime.seconds);
+  }
+  console.log(savedTime.seconds);
+} catch {
+  console.log('The first viewing');
+}
+
+player.on('timeupdate', throttle(data => {
+    try {
+      localStorage.setItem(LOCALSTORAGE_KEY, `${JSON.stringify(data)}`);
+    } catch (error) {
+      console.log(error);
+    }
+}, 1000));
